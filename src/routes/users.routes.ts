978e215e431aefc0686cc0fb17c3826f97e0e353
Francielli Rodrigues <contato.franciellirodrigues@outlook.com>
2023-06-01 20:52:31 -0300

    finished project

---
+++ src/routes/users.routes.ts
@@ -0,0 +1,29 @@
+import { Router } from 'express'
+import { UsersController } from '../controllers/UsersController'
+import { upload } from '../config/multer'
+
+class UsersRoutes {
+  private router: Router
+  private usersController: UsersController
+  constructor() {
+    this.router = Router()
+    this.usersController = new UsersController()
+  }
+  getRoutes(): Router {
+    this.router.post('/', this.usersController.store.bind(this.usersController))
+    this.router.put(
+      '/',
+      upload.single('avatar_url'),
+      this.usersController.update.bind(this.usersController)
+    )
+
+    this.router.post(
+      '/auth',
+      this.usersController.auth.bind(this.usersController)
+    )
+
+    return this.router
+  }
+}
+
+export { UsersRoutes }
